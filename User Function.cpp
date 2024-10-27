0920dfe6bf5e92e4f0f50ed5d8f8effc58ae589e
Arnold Gavrael Bonardo Situmorang <arnoldgavraelbonardo@mail.ugm.ac.id>
2024-10-27 15:26:05 +0700

    FINAL

--- User Function.cpp
+++ User Function.cpp
@@ -3,10 +3,17 @@
 Arnold Gavrael - 539797 - Kelas B
 
 Cara Kerja:
-1. Pengguna akan memasukkan koordinat titik pusat (x1 dan x2) serta titik ujung lingkaran (x2 y2).
+1. Pengguna akan memasukkan koordinat titik pusat (x1 dan y2) serta titik ujung lingkaran (x2 y2).
 2. Program akan menghitung jarak antara dua titik tersebut dan mendefinisikannya sebagai jari-jari lingkaran.
 3. Program akan menghitung diameter, keliling, dan luas lingkaran menggunakan jari-jari tersebut.
 4. Program akan menampilkan semua data yang telah dihitung di terminal.
+
+Opsional:
+1. Pengguna akan memasukkan koordinat titk ketiga (x3 dan y3).
+2. Program akan membandingkan jarak antara titik pertama dan kedua (j2) dan jarak anatara titik pertama dan ketiga (j3).
+3. Jika j3 kurang dari j2, titik ada di dalam lingkaran.
+4. Jika j3 sama dengan dari j2, titik ada di ujung lingkaran.
+5. Jika j3 lebih dari j2, titik ada di luar lingkaran.
 
 */
 
@@ -17,40 +24,70 @@ const double pi = 3.1416;
 
 // Fungsi jarak
 double jarak(double x1, double y1, double x2, double y2) {
-    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)); }
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
 
 // Fungsi jari-jari
 double jari(double x1, double y1, double x2, double y2) {
-    return jarak(x1, y1, x2, y2); }
+    return jarak(x1, y1, x2, y2);
+}
 
 // Fungsi keliling
-double keliling(double jari) {
-    return 2 * pi * jari; }
+double keliling(double jari) { 
+    return 2 * pi * jari;
+}
 
 // Fungsi luas
-double luas(double jari) {
-    return pi * jari * jari; }
+double luas(double jari) { 
+    return pi * jari * jari;
+}
 
 // Fungsi utama
 int main() {
-    double x1, y1, x2, y2;
+
+    // Deklarasi variabel
+    double x1, y1, x2, y2, x3, y3;
+    char pilihan;
 
     // Input dari pengguna
     cout << "Masukkan koordinat titik pusat lingkaran (x1 y1): ";
     cin >> x1 >> y1;
     cout << "Masukkan koordinat titik ujung lingkaran (x2 y2): ";
     cin >> x2 >> y2;
-    
+
     // Mendefinisikan jari-jari
-    double j = jari(x1, y1, x2, y2);
+    double j2 = jari(x1, y1, x2, y2);
 
     // Hasil
-    cout << "Jari-jari: " << j << "\n";
-    cout << "Diameter: " << 2 * j << "\n";
-    cout << "Keliling: " << keliling(j) << "\n";
-    cout << "Luas: " << luas(j) << "\n";
+    cout << "\nJari-jari: " << j2 << "\n";
+    cout << "Diameter: " << 2 * j2 << "\n";
+    cout << "Keliling: " << keliling(j2) << "\n";
+    cout << "Luas: " << luas(j2) << "\n";
 
-    return 0; }
+    // Input titik ketiga (opsional)
+    cout << "\nMasukkan titik ketiga? (y/n): ";
+    cin >> pilihan;
+
+    while (pilihan == 'Y' || pilihan == 'y') {
+
+        cout << "\nMasukkan titik yang akan dicek (x3 y3): ";
+        cin >> x3 >> y3;
+
+        // Mendefinisikan jarak anatar titik pusat dengan titik ketiga
+        double j3 = jari(x1, y1, x3, y3);
+
+        // Cek apakah titik ada di dalam lingkaran
+        if (j3 < j2) { cout << "Titik berada di dalam lingkaran.\n"; } 
+        else if (j3 == j2) { cout << "Titik berada di ujung lingkaran.\n"; }
+        else { cout << "Titik berada di luar lingkaran.\n"; }
+
+        // Tanyakan lagi apakah ingin memasukkan titik ketiga
+        cout << "\nIngin memeriksa titik lain? (y/n): ";
+        cin >> pilihan;
+    }
+
+    return 0;
+}
 
 /*
 
