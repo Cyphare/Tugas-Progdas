079e1344ed0f6bda8fe1fdf91ceada989ea70463
Arnold Gavrael Bonardo Situmorang <arnoldgavraelbonardo@mail.ugm.ac.id>
2024-10-28 17:48:49 +0700

    FINAL

--- Struct (User Input).cpp
+++ Struct (User Input).cpp
@@ -31,14 +31,16 @@ struct studentType {
 };
 
 // Function prototypes
-void readStudentData(studentType students[], int size); // Function to input student data and their number grades
-void assignGrades(studentType students[], int size); // Function to assign alphabetical grades based on number grades
-int findHighestScore(studentType students[], int size); // Function to find the highest score from all the student's grades
-void printHighestScorers(studentType students[], int size, int highestScore); // Function to print the highest scoring student's name and their grades
-void printAllStudents(studentType students[], int size); // Function to print all the students name and grades
+void readStudentData(studentType students[], int size); 
+void assignGrades(studentType students[], int size); 
+int findHighestScore(studentType students[], int size); 
+void printAllStudents(studentType students[], int size); 
+void printHighestScorers(studentType students[], int size, int highestScore); 
 
 int main() {
-    studentType students[20];  // Array for 20 students
+
+    // Set an array for 20 students
+    studentType students[20];  
 
     // Function calls
     readStudentData(students, 20);
@@ -50,40 +52,41 @@ int main() {
     return 0;
 }
 
-// Function to read students' data
+// Function to input student data and their number grades
 void readStudentData(studentType students[], int size) {
     for (int i = 0; i < size; i++) {
         cout << "Enter first name, last name, and test score for student " << i + 1 << ": ";
         cin >> students[i].studentFName >> students[i].studentLName >> students[i].testScore;
+        // You enter the data as follows: First name, Last name, Score
     }
 }
 
-// Function to assign grades based on score
+// Function to assign alphabetical grades based on number grades
 void assignGrades(studentType students[], int size) {
     for (int i = 0; i < size; i++) {
-        if (students[i].testScore >= 90) students[i].grade = 'A';
-        else if (students[i].testScore >= 80) students[i].grade = 'B';
-        else if (students[i].testScore >= 70) students[i].grade = 'C';
-        else if (students[i].testScore >= 60) students[i].grade = 'D';
-        else if (students[i].testScore >= 50) students[i].grade = 'E'
-        else students[i].grade = 'F';
+        if (students[i].testScore >= 90) {students[i].grade = 'A';} // 90-100 gets an A
+        else if (students[i].testScore >= 80) {students[i].grade = 'B';} // 80-89 gets a B
+        else if (students[i].testScore >= 70) {students[i].grade = 'C';} // 70-79 gets a C
+        else if (students[i].testScore >= 60) {students[i].grade = 'D';} // 60-69 gets a D
+        else if (students[i].testScore >= 50) {students[i].grade = 'E';} // 50-59 gets an E
+        else {students[i].grade = 'F';} // 0-49 gets an F
     }
 }
 
-// New function to print all students' scores
+// Function to print all the students name and grades
 void printAllStudents(studentType students[], int size) {
     cout << "\nAll Students' Scores:\n\n";
-    cout << left << setw(15) << "Last Name" << setw(15) << "First Name" << setw(10) << "Score" << "Grade\n";
-    cout << "---------------------------------------------\n";
+    cout << left << setw(20) << "Name" << setw(10) << "Score" << "Grade\n";
+    cout << "------------------------------------\n";
     for (int i = 0; i < size; i++) {
-        cout << left << setw(15) << (students[i].studentLName + ",")  // Add comma after last name
-             << setw(15) << students[i].studentFName 
+        cout << left << setw(20) << (students[i].studentLName + ", " + students[i].studentFName)
              << setw(10) << students[i].testScore 
              << students[i].grade << '\n';
+        // "setw" sets a width at the start of a word, so that any word typed after is justified
     }
 }
 
-// Function to find the highest score
+// Function to find the highest score from all of the student
 int findHighestScore(studentType students[], int size) {
     int highest = students[0].testScore;
     for (int i = 1; i < size; i++) {
@@ -92,16 +95,19 @@ int findHighestScore(studentType students[], int size) {
         }
     }
     return highest;
+    // It sets the first student's grade as the highest and compares it to the next
+    // If it is higher, it sets that as the highest.
+    // In the end, the highest score will be foung (bubble sort).
 }
 
-// Function to print names of students with the highest score
+// Function to print the highest scoring student's name and their grades
 void printHighestScorers(studentType students[], int size, int highestScore) {
     cout << "\nStudents with the highest score:\n";
     for (int i = 0; i < size; i++) {
         if (students[i].testScore == highestScore) {
             cout << students[i].studentLName << ", "
                  << students[i].studentFName << " - " 
-                 << students[i].testScore << " (" << students[i].grade << ")" << '\n';
+                 << students[i].testScore << " (" << students[i].grade << ")\n";
         }
     }
 }
